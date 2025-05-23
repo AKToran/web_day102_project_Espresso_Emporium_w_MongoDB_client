181f3dee10a5c4d6f52d3947596e20a5ff846fdf
AKToran <aktoran60@gmail.com>
2025-05-23 20:24:26 +0600

    create user in firebase

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -4,16 +4,26 @@ import AuthContext from '../Context/AuthContext';
 const SignUp = () => {
 
   const {createUser} = use(AuthContext) 
-  console.log(createUser);
 
   const handleSignUp = e =>{
     e.preventDefault();
     const form = e.target;
     const formData = new FormData(form);
-    const email = formData.get('email');
-    const password = formData.get('password');
 
-    console.log(email, password);
+    const { email, password, ...newUser} = Object.fromEntries(formData.entries());
+    console.log(newUser);
+
+    //create user in firebase:
+    createUser(email, password)
+    .then(res =>{
+      console.log(res.user);
+      // save profile info in the database
+
+
+    })
+    .catch(err =>{
+      console.log(err);
+    })
 
   }
 
@@ -22,8 +32,19 @@ const SignUp = () => {
       <div className="card-body items-center">
         <h1 className="text-3xl font-bold">Sign Up now!</h1>
         <form onSubmit={handleSignUp} className="fieldset">
+
+          <label className="label">Name</label>
+          <input type="text" name="name" className="input" placeholder="Your Name" />
+
           <label className="label">Email</label>
           <input type="email" name="email" className="input" placeholder="Email" />
+
+          <label className="label">Address</label>
+          <input type="text" name="address" className="input" placeholder="Address" />
+
+          <label className="label">Phone</label>
+          <input type="number" name="phone" className="input" placeholder="Phone" />
+
           <label className="label">Password</label>
           <input type="password" name="password" className="input" placeholder="Password" />
 
