6f138a9884d0b24fe63e7929775d16476b123569
AKToran <aktoran60@gmail.com>
2025-05-24 22:55:37 +0600

    used sweet alert and handle delete

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,17 +1,48 @@
-import React, { useState } from 'react';
-import { useLoaderData } from 'react-router';
+import React, { useState } from "react";
+import { useLoaderData } from "react-router";
+import Swal from "sweetalert2";
 
 const Users = () => {
   const initialUsers = useLoaderData();
   const [users, setUsers] = useState(initialUsers);
 
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+      }
+    });
+  };
+
   return (
     <div>
-      <h2 className='text-3xl'>all users: {initialUsers.length}</h2>
+      <h2 className="text-3xl">all users: {initialUsers.length}</h2>
       <div>
-        {
-          users.map(user => <div key={user._id}> <p>{user.name}</p> </div>)
-        }
+        {users.map((user) => (
+          <div className="m-4" key={user._id}>
+            {" "}
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+            <p>{user.address}</p>
+            <button className="btn">V</button>
+            <button className="btn">E</button>
+            <button onClick={() => handleDelete(user._id)} className="btn">
+              X
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
