5ed45954f51b49e2b117e7f5bcd7bb87063cc819
AKToran <aktoran60@gmail.com>
2025-06-07 23:19:14 +0600

    users2 component

---
+++ src/Components/Users2.jsx
@@ -0,0 +1,63 @@
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import Swal from "sweetalert2";
+
+const Users2 = () => {
+
+
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3000/users/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.acknowledged) {
+              // const remainingUsers = users.filter((user) => user._id !== id);
+              // setUsers(remainingUsers);
+
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
+
+
+  return (
+    <div>
+      {/* <h2 className="text-3xl">all users: {initialUsers.length}</h2> */}
+      <div>
+        {users.map((user) => (
+          <div className="m-4" key={user._id}>
+            {" "}
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+            <p>{user.address}</p>
+            <button className="btn">V</button>
+            <button className="btn">E</button>
+            <button onClick={() => handleDelete(user._id)} className="btn">
+              X
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Users2;
--- src/main.jsx
+++ src/main.jsx
@@ -11,47 +11,63 @@ import SignUp from "./Components/SignUp.jsx";
 import AuthProvider from "./Context/AuthProvider.jsx";
 import Users from "./Components/Users.jsx";
 
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import Users2 from "./Components/Users2.jsx";
+
 const router = createBrowserRouter([
   {
     path: "/",
     Component: MainLayout,
     hydrateFallbackElement: <p>Loading coffees...</p>,
     children: [
-      { 
-        index: true, 
-        loader: () => fetch('http://localhost:3000/coffees'),
-        Component: Home 
+      {
+        index: true,
+        loader: () => fetch("http://localhost:3000/coffees"),
+        Component: Home,
       },
-      { 
-        path: "addCoffee", 
-        Component: AddCoffee 
+      {
+        path: "addCoffee",
+        Component: AddCoffee,
       },
-      { 
+      {
         path: "updateCoffee/:id",
-        loader: ({params})=> fetch(`http://localhost:3000/coffees/${params.id}`),
-        Component: UpdateCoffee 
+        loader: ({ params }) =>
+          fetch(`http://localhost:3000/coffees/${params.id}`),
+        Component: UpdateCoffee,
       },
       {
         path: "signin",
-        Component: SignIn
+        Component: SignIn,
       },
       {
         path: "signup",
-        Component: SignUp
+        Component: SignUp,
       },
       {
-        path:"users",
-        loader: ()=> fetch('http://localhost:3000/users'),
-        Component: Users
-      }
+        path: "users",
+        loader: () => fetch("http://localhost:3000/users"),
+        Component: Users,
+      },
+      {
+        path: "users2",
+        Component: Users2,
+      },
     ],
   },
 ]);
 
+// Create a client
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
